15f0f895dcda07c27d2a23320611eb111ce5c428
AizikAizik <isaac.ogunleye@hotmail.com>
2021-04-27 03:22:28 -0700

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWrl2TwSnjK17NDrL-e_HitRSgWlonpfY",
+    authDomain: "snapchat-web-clone.firebaseapp.com",
+    projectId: "snapchat-web-clone",
+    storageBucket: "snapchat-web-clone.appspot.com",
+    messagingSenderId: "326315222871",
+    appId: "1:326315222871:web:343af628fef0eb6d724012"
+};
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const storage = firebase.storage();
+
+const auth = firebase.auth();
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export {
+    provider,
+    storage,
+    auth,
+    db
+}
