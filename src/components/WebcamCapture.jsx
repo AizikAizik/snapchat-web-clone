d624145c7f5558f3a0b0d8b5cf29f803406ac2c8
AizikAizik <isaac.ogunleye@hotmail.com>
2021-04-27 08:29:59 -0700

    removed boiler plate and added webcam component on the screen

---
+++ src/components/WebcamCapture.jsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useRef, useState } from "react";
+import Webcam from "react-webcam";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+
+const videoConstraints = {
+  width: 250,
+  height: 400,
+  facingMode: "user",
+};
+
+const WebcamCapture = () => {
+  const webcamRef = useRef(null);
+
+  const capture = useCallback(() => {
+    const imgSrc = webcamRef.current.getScreenshot();
+    
+  }, [webcamRef]);
+
+  return (
+    <div className="webcamCapture">
+      <Webcam
+        audio={false}
+        height={videoConstraints.height}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={videoConstraints.width}
+        videoConstraints={videoConstraints}
+      />
+      
+      <RadioButtonUncheckedIcon
+        className="webcamCapture__button"
+        onClick={capture}
+        fontSize="large"
+      />
+    </div>
+  );
+};
+
+export default WebcamCapture;
